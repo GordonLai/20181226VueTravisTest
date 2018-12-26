643d5227e23b11c4e338c7799668792b0e4ae895
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-26 13:27:22 +0000

    Deploy GordonLai/20181226VueTravisTest to github.com/GordonLai/20181226VueTravisTest.git:gh-pages

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import HelloWorld from '@/components/HelloWorld'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'hash', // history | hash
   base: '/20181226VueTravisTest/',
   routes: [
     {
